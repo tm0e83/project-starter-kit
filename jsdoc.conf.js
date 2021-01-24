6917b7bbfabb5f71d0276947f21a54dfb66e32ca
Timo Szyszka <tm0e83@gmail.com>
2021-01-24 11:01:40 +0100

    added index; removed unused files

---
+++ jsdoc.conf.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = {
+  "opts": {
+    "destination": "./docs/",
+    "recurse": true
+  },
+  "source": {
+    "include": [
+      "../admin/src/js",
+      "../analytics/src/js",
+      "../application/src/js",
+      "../calendarium/src/js",
+      "../mailmanager/src/js",
+      "../mundus-agri/src/js",
+      "../novella-agri/src/js",
+      "../ostentus/src/js",
+      "../website/src/js",
+    ]
+  }
+};
